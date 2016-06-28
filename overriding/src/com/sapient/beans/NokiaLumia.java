df37fa5d334500616d635df46e8ccd981c7ddd39
bkhan1 <bkhan1@NOILAB10530.sapient.com>
2016-06-28 14:09:34 +0530

    shasha grey is great

---
+++ overriding/src/com/sapient/beans/NokiaLumia.java
@@ -0,0 +1,11 @@
+package com.sapient.beans;
+
+public class NokiaLumia extends Nokia1100 {
+	public void doConverse(){
+		System.out.println("3g converse");
+	}
+	public void captureVideo(){
+		System.out.println("capture video");
+	}
+
+}
